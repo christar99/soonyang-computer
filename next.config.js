3f45a592c17d7ae13ef4e52d8a3920b14ca4d6bc
이동아 <christar99@naver.com>
2023-02-07 12:35:33 +0900

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const path = require("path");
+
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: []
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")]
+    }
+};
