85b1913206c2a4d8e4097b278b18fef0944107f1
Dong-A <53134924+christar99@users.noreply.github.com>
2023-02-10 23:08:31 +0900

    Delete api directory

--- api/README.md
+++
@@ -1,7 +0,0 @@
-# API 서버, DB 실행
-
-- docker-compose up -d
-
-# Swagger 문서
-
-- localhost:3000/docs
--- api/src/user/dto/update-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateUserReqDto } from './create-user.dto';
-import { PartialType } from '@nestjs/swagger';
-
-export class UpdateUserReqDto extends PartialType(CreateUserReqDto) {}
